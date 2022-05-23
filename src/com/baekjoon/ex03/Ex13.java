5a0a46d99a46e223e130d3208f58a4c38b8c6cfd
jongbin-choi <kooma1123@naver.com>
2022-05-23 09:45:51 +0900

    2022-05-22

---
+++ src/com/baekjoon/ex03/Ex13.java
@@ -0,0 +1,24 @@
+package com.baekjoon.ex03;
+
+import java.util.Scanner;
+
+public class Ex13 {
+	//10951번 문제
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+			
+			while(sc.hasNextInt()) {
+				
+				int num1 = sc.nextInt();
+				int num2 = sc.nextInt();
+				int sum = num1 + num2;
+				
+				System.out.println(sum);
+			}
+			
+	}
+
+}
