dba1e9990311fe6d2e3080b932b2afea41db38a4
jongbin-choi <kooma1123@naver.com>
2022-06-06 15:14:49 +0900

    백준

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,9 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+//10818번 문제
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex02.java
@@ -0,0 +1,11 @@
+package com.javaex.ex04;
+
+public class Ex02 {
+//2562번 문제
+	public static void main(String[] args) {
+
+		// N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+		
+	}
+
+}
