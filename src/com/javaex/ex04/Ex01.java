6430b565151c11ddc8729b859f49d158730090c7
jongbin-choi <kooma1123@naver.com>
2022-06-06 17:35:17 +0900

    백준 배열

--- src/com/javaex/ex04/Ex01.java
+++ src/com/javaex/ex04/Ex01.java
@@ -1,9 +1,28 @@
 package com.javaex.ex04;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ex01 {
 //10818번 문제
 	public static void main(String[] args) {
-
+	
+		Scanner sc = new Scanner(System.in);
+		
+		int count = sc.nextInt();
+		int[] cArray = new int [count];	//count의 개수만큼 배열공간 생성
+		
+		
+		
+		
+		for(int i = 0; i < cArray.length; i++) {
+			cArray[i] = sc.nextInt();
+		System.out.println(cArray[i]);
+		
+		}//for문 끝
+		Arrays.sort(cArray);//오름차순정렬 메소드
+		System.out.println(cArray[0] + " " + cArray[count-1]);
+		
 	}
 
 }
